c45d329e001b5144814cb4f0f38978ae2c0afd10
Penguin <jimmy_441588177@126.com>
2016-05-12 00:26:22 +0800

    Update CenterSrv.c

--- CenterSrv/CenterSrv.c
+++ CenterSrv/CenterSrv.c
@@ -19,14 +19,19 @@
 #define PERM S_IRUSR|S_IWUSR
 //Define
 //----------
-typedef struct msgbuf{      	/* 消息定义的参照格式 */
-  	long mtype;       		/* 消息类型（大于0的长整数）  */
-	char mtext[BUFFER+1]; 			/*消息正文*/
+typedef struct msgbuf{ 		/* 消息定义的参照格式 */
+  	long mtype;       						/* 消息类型（大于0的长整数）  */
+	char mtext[BUFFER+1]; 		/*消息正文*/
 }msgbuf;
 //Message Buffer Struct
 typedef struct msqid_ds msgque;
 //Message Queue Struct
-
+typedef struct USER_TABLE{ 
+  	char name[20];				//用户名
+  	int pid;								//由 CenterSrv 分配的ID
+  	int sendsrvid;				//Clinet使用的 Sendsrv ID
+}users;
+//User Table Struct
 //----------
 key_t msgkey=0;
 msgbuf msg;
@@ -51,7 +56,26 @@ int init()
 	return 0;
 }
 
-int client_add()
+int uninit()
+{
+	if(msgctl(msgid, IPC_RMID, 0) == -1)
+    {
+        fprintf(stderr, "Remove MessageQueue Error%s \n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    exit(EXIT_SUCCESS);
+}
+
+int process()
+{
+	while(1)
+	{
+		msgrcv(msgid,&msg,sizeof(struct msgbuf),1,0);
+	}
+	return 0;
+}
+
+int client_pidsend()
 {
 	return 0;
 }
