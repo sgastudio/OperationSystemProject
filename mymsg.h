891b00002088fbdcd813d1f3f621aa821658ddad
Penguin <jimmy_441588177@126.com>
2016-05-12 17:47:52 +0800

    Update mymsg.h

--- mymsg.h
+++ mymsg.h
@@ -17,18 +17,21 @@
 #define MSG_FILE "CenterSrv.c"
 #define BUFFER 255
 #define PERM S_IRUSR|S_IWUSR
+#define PERSON_PER_SERVER 5
 //Define
 //----------
-typedef struct msgbuf{ 		/* 消息定义的参照格式 */
-  long mtype;       						/* 消息类型（大于0的长整数）  */
+typedef struct MSG_BUF{ 		/* 消息定义的参照格式 */
+  	long mtype;       		/* 消息类型（大于0的长整数）  */
 	char mtext[BUFFER+1]; 		/*消息正文*/
 }msgbuf;
 //Message Buffer Struct
 typedef struct msqid_ds msgque;
 //Message Queue Struct
 typedef struct USER_TABLE{ 
-  char name[20];				//用户名
-  int pid;								//由 CenterSrv 分配的ID
-  int sendsrvid;				//Clinet使用的 Sendsrv ID
+	int pid;			//由 CenterSrv 分配的ID
+  	char name[20];			//用户名
+  	int sendsrvid;			//Clinet使用的 Sendsrv ID
+  	char location[4096];		//消息队列文件地址
+  	struct USER_TABLE * next;	//下一记录地址
 }users;
 //User Table Struct
